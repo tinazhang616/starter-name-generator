5c561579cafc57ea354e13fb4424e0f340b1c597
Tina <tinazhang616@gmail.com>
2023-06-15 09:23:31 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch('https://starter-name-generator-r9kn.onrender.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
